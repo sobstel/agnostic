198fc31831f89ad9215dcffab02082a626a9ab7c
Przemek Sobstel <przemek@sobstel.org>
2014-09-11 20:14:38 +0200

    films_with_actors example: add orderby condition...
    ...to show it's possible to use original doctrine query
    builder interface

--- tests/playground/catalog/films_with_actors.php
+++ tests/playground/catalog/films_with_actors.php
@@ -3,5 +3,7 @@ return $manager
     ->query('film')
     ->with('language')
     ->with('actors')
+    ->addOrderBy('replacement_cost', 'DESC')
+    ->addOrderBy('rental_rate', 'DESC')
     ->setMaxResults(30)
     ->fetch();
